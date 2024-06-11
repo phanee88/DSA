abc0fa25abfb14114e3bbb7e13bc7f3b092d8016
Phaneendra <phanee.c@gmail.com>
2024-06-11 07:11:01 +0530

    formatted

--- src/com/dsa/twopointer/SumOfThree.java
+++ src/com/dsa/twopointer/SumOfThree.java
@@ -6,41 +6,36 @@ public class SumOfThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		int [] nums = {1, 3, 4, 6, 8, 20};
-		int target=31;
 
-		boolean res=isSumOfThree(nums,target);
-		System.out.println("result is "+res);
+		int[] nums = { 1, 3, 4, 6, 8, 20 };
+		int target = 31;
+
+		boolean res = isSumOfThree(nums, target);
+		System.out.println("result is " + res);
 	}
-	
-	public static  boolean isSumOfThree(int [] nums,int target)
-	{
+
+	public static boolean isSumOfThree(int[] nums, int target) {
 		Arrays.sort(nums);
-		
-		for(int i=0;i<nums.length-2;i++)
-		{
-			
-			int start=i+1;
-			int end=nums.length-1;
-			
-			
-			while(start<end)
-			{
-				int sum = nums[i]+nums[start]+nums[end];
-				if(sum==target) {
+
+		for (int i = 0; i < nums.length - 2; i++) {
+
+			int start = i + 1;
+			int end = nums.length - 1;
+
+			while (start < end) {
+				int sum = nums[i] + nums[start] + nums[end];
+				if (sum == target) {
 					return true;
-							
-				}else if (sum<target) {
+
+				} else if (sum < target) {
 					start++;
-				}else {
-					end --;
+				} else {
+					end--;
 				}
 			}
-			
-			
+
 		}
-		
+
 		return false;
 	}
 
